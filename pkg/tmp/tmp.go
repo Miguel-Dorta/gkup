014ea33ab6480245d7d20718c374d1f50cef2875
Miguel-Dorta <contact@migueldorta.com>
2019-07-05 22:12:33 +0100

    Remove unused package

--- pkg/tmp/tmp.go
+++
@@ -1,7 +0,0 @@
-// Package tmp is intended to be used as a temporal develop directory.
-// All it's content will not be here for the post-alpha versions.
-package tmp
-
-var (
-	ReadSymLink = false
-)
