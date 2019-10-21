7b484390f053cd16feb4ee8cf94e9c73c4bbbe34
Miguel-Dorta <contact@migueldorta.com>
2019-10-21 20:59:56 +0100

    Add more hashing algorithms

--- pkg/repository/actions/check/hash.go
+++ pkg/repository/actions/check/hash.go
@@ -1,8 +1,12 @@
 package check
 
 import (
+	"crypto/md5"
+	"crypto/sha1"
 	"crypto/sha256"
+	"crypto/sha512"
 	"errors"
+	"golang.org/x/crypto/sha3"
 	"hash"
 	"io"
 	"os"
@@ -13,6 +17,16 @@ func getHash(algorithm string) (hash.Hash, error) {
 	switch strings.ToLower(algorithm) {
 	case "sha256":
 		return sha256.New(), nil
+	case "md5":
+		return md5.New(), nil
+	case "sha1":
+		return sha1.New(), nil
+	case "sha512":
+		return sha512.New(), nil
+	case "sha3-256":
+		return sha3.New256(), nil
+	case "sha3-512":
+		return sha3.New512(), nil
 	default:
 		return nil, errors.New("hash algorithm unknown")
 	}
